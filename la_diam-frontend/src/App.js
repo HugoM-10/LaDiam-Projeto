1663971e68c746f60bd4c00cd2d9cba41a96089d
Manuel Almeida <66184545+manu183@users.noreply.github.com>
2025-05-11 17:34:20 +0100

    Refactor App component structure to improve readability and encapsulate routing logic

--- la_diam-frontend/src/App.js
+++ la_diam-frontend/src/App.js
@@ -1,27 +1,38 @@
+import { useContext } from "react";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import { Routes, Route } from "react-router-dom";
 import LoginManager from "./Pages/LoginManager/LoginManager";
 import Signup from "./Pages/LoginManager/Signup";
-import UserProvider from "./UserContext";
 import Profile from "./Pages/Profile/Profile";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import HomePage from "./Pages/HomePage/HomePage";
+import { UserProvider, UserContext } from "./UserContext";
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <UserProvider>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginManager />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/profile" element={<Profile/>} />
-        </Routes>
-        </UserProvider>
-        <Footer />
+        <AppContent />
+      </UserProvider>
+    </div>
+  );
+}
+
+function AppContent() {
+  const { isLoggedIn } = useContext(UserContext);
+
+  return (
+    <div className="App">
+      <Header isLoggedIn={isLoggedIn} />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginManager />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
