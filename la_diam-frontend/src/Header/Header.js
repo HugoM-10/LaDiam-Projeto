572f2da56eecefd3d20c8da57358fdc7d8a7f874
Manuel Almeida <66184545+manu183@users.noreply.github.com>
2025-05-11 17:33:55 +0100

    Refactor Header component and CSS for improved layout and styling

--- la_diam-frontend/src/Header/Header.js
+++ la_diam-frontend/src/Header/Header.js
@@ -1,25 +1,43 @@
 import React from "react";
-import "./Header.css"; // We'll create this CSS file next
+import "./Header.css";
 import { useNavigate, Link } from "react-router-dom";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { useContext } from "react";
+import { FaShoppingCart } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
+import { FaRegUserCircle } from "react-icons/fa";
+import { IoLogOutOutline } from "react-icons/io5";
 
-function Header() {
+function Header({isLoggedIn}) {
   const navigate = useNavigate();
 
-  const handleLoginClick = () => {
-    navigate("/login");
-  };
   return (
     <header className="app-header">
-      <div className="header-logo">
+      <div className="header-content">
         <Link to="/">
           <img src="Logo-LaDiam.png" alt="Logo" className="logo-image" />
         </Link>
-      </div>
 
-      <div className="header-actions">
-        <button className="login-button" onClick={handleLoginClick}>
-          Entrar
-        </button>
+        <Button>Encomendar Agora!</Button>
+
+        <Button>Promoções</Button>
+
+        <Button>Sobre nós!</Button>
+
+        {isLoggedIn ? (
+          <div className="header-icons">
+            <FaShoppingCart size={30} color="white" />
+            <MdOutlineEmail size={30} color="white" />
+            <FaRegUserCircle size={30} color="white" />
+            <IoLogOutOutline size={30} color="white" />
+          </div>
+        ) : (
+          <div>
+            <Link to="/login">
+              <Button color="danger">Área de Cliente</Button>
+            </Link>
+          </div>
+        )}
       </div>
     </header>
   );
