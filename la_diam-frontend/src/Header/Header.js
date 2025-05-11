0790f85cf2c328c7fba7ed6f5efbfe6624d36369
Manuel Almeida <66184545+manu183@users.noreply.github.com>
2025-05-11 21:40:53 +0100

    Update Header component with responsive navbar and enhance styles; modify CSS variables for consistency

--- la_diam-frontend/src/Header/Header.js
+++ la_diam-frontend/src/Header/Header.js
@@ -1,43 +1,103 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import "./Header.css";
-import { useNavigate, Link } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 import { FaShoppingCart } from "react-icons/fa";
 import { MdOutlineEmail } from "react-icons/md";
 import { FaRegUserCircle } from "react-icons/fa";
 import { IoLogOutOutline } from "react-icons/io5";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  Button,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+} from "reactstrap";
+import { UserContext } from "../UserContext";
 
-function Header({isLoggedIn}) {
-  const navigate = useNavigate();
+function Header() {
+  const { isLoggedIn, logout } = useContext(UserContext);
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggle = () => setIsOpen(!isOpen);
 
   return (
     <header className="app-header">
-      <div className="header-content">
-        <Link to="/">
+      <Navbar className="navbar" expand="md">
+        <NavbarBrand href="/">
           <img src="Logo-LaDiam.png" alt="Logo" className="logo-image" />
-        </Link>
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="nav-items" navbar>
+            <NavItem className="nav-item">
+              <NavLink href="">
+                <Button color="danger" className="nav-button">
+                  Encomendar agora!
+                </Button>
+              </NavLink>
+            </NavItem>
+            <NavItem className="nav-item">
+              <NavLink href="">
+                <Button color="danger" className="nav-button">
+                  Promoções
+                </Button>
+              </NavLink>
+            </NavItem>
+            <NavItem className="nav-item">
+              <NavLink href="">
+                <Button color="danger" className="nav-button">
+                  Sobre nós!
+                </Button>
+              </NavLink>
+            </NavItem>
+            {isLoggedIn ? (
+              <div className="header-icons">
+                <NavItem>
+                  <NavLink>
+                    <FaShoppingCart className="icon" />
+                  </NavLink>
+                </NavItem>
 
-        <Button variant="danger">Encomendar Agora!</Button>
+                <NavItem>
+                  <NavLink>
+                    <MdOutlineEmail className="icon" />
+                  </NavLink>
+                </NavItem>
 
-        <Button variant="danger">Promoções</Button>
-
-        <Button variant="danger">Sobre nós!</Button>
-
-        {isLoggedIn ? (
-          <div className="header-icons">
-            <FaShoppingCart size={30} color="white" />
-            <MdOutlineEmail size={30} color="white" />
-            <FaRegUserCircle size={30} color="white" />
-            <IoLogOutOutline size={30} color="white" />
-          </div>
-        ) : (
-          <div>
-            <Link to="/login">
-              <Button color="warning">Área de Cliente</Button>
-            </Link>
-          </div>
-        )}
-      </div>
+                <NavItem>
+                  <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                      <FaRegUserCircle className="icon" />
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                      <DropdownItem>Perfil</DropdownItem>
+                      <DropdownItem>Histórico</DropdownItem>
+                      <DropdownItem>Carteira</DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </NavItem>
+                <NavItem>
+                  <IoLogOutOutline className="icon" onClick={logout} />
+                </NavItem>
+              </div>
+            ) : (
+              <div>
+                <Link to="/login">
+                  <Button className="login-button">Área de Cliente</Button>
+                </Link>
+              </div>
+            )}
+          </Nav>
+        </Collapse>
+      </Navbar>
     </header>
   );
 }
