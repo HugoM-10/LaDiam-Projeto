32b3639c7bf82b5c6876de3589b43e7950cf80d3
Manuel Almeida <66184545+manu183@users.noreply.github.com>
2025-05-12 11:31:44 +0100

    Refactor order views and update URL patterns for clarity and consistency

--- la_diam_backend/pizzaria/order_views.py
+++ la_diam_backend/pizzaria/order_views.py
@@ -10,22 +10,23 @@ from .serializers import OrderSerializer, OrderItemSerializer
 
 @api_view(['GET'])
 @permission_classes([IsAdminUser])
-def get_orders_view(request):
+def get_all_orders_view(request):
     orders = Order.objects.all()
     serializer = OrderSerializer(orders, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
-def get_own_orders_view(request):
+def get_my_orders_view(request):
     user = request.user
     orders = Order.objects.filter(user=user)
     serializer = OrderSerializer(orders, many=True)
     return Response(serializer.data)
 
+
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
-def edit_own_order_view(request):
+def get_order_view(request):
     order_id = request.data.get('id')
     order = Order.objects.get(id=order_id)
     
@@ -34,6 +35,23 @@ def edit_own_order_view(request):
     
     # Check if the order belongs to the user
     if order.user != request.user:
+        return Response({'error': 'You do not have permission to view this order'}, status=status.HTTP_403_FORBIDDEN)
+
+    serializer = OrderSerializer(order)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def edit_order_view(request):
+    order_id = request.data.get('id')
+    order = Order.objects.get(id=order_id)
+    
+    if not order:
+        return Response({'error': 'Order not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    # Check if the order belongs to the user and if the user is not an admin
+    if order.user != request.user and not request.user.is_staff:
         return Response({'error': 'You do not have permission to edit this order'}, status=status.HTTP_403_FORBIDDEN)
 
     quantity = request.data.get('quantity')
@@ -53,8 +71,8 @@ def delete_order_view(request):
     order_id = request.data.get('id')
     order = Order.objects.get(id=order_id)
 
-    # Check if the order belongs to the user
-    if order.user != request.user:
+    # Check if the order belongs to the user and if the user is not an admin
+    if order.user != request.user and not request.user.is_staff:
         return Response({'error': 'You do not have permission to delete this order'}, status=status.HTTP_403_FORBIDDEN)
 
     if not order:
