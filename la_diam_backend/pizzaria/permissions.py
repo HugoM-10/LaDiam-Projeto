df96b445acc7be0388372c1ac901dab808a24e1a
Manuel Almeida <66184545+manu183@users.noreply.github.com>
2025-05-13 20:04:28 +0100

    Refactor permission decorators to include user authentication checks

--- la_diam_backend/pizzaria/permissions.py
+++ la_diam_backend/pizzaria/permissions.py
@@ -1,31 +1,35 @@
 from django.contrib.auth.decorators import user_passes_test
+from django.http import JsonResponse
+from django.contrib.auth.models import User, Group
+
 
 #Check if the user is a "Vendedor" or "Gestor"
 def is_vendedor_or_gestor(user):
-    return user.groups.filter(name__in=['Vendedor', 'Gestor']).exists()
+    return user.is_superuser or (user.is_authenticated and user.groups.filter(name__in=['Vendedor', 'Gestor']).exists())
 
 
 #Check if the user is a "Gestor"
 
 def is_gestor(user):
-    return user.groups.filter(name='Gestor').exists()
+    return user.is_superuser or (user.is_authenticated and user.groups.filter(name='Gestor').exists())
 
 
-#Decorator to check if the user is a "Vendedor" or "Gestor"
-
+# Decorator to check if the user is "Vendedor" or "Gestor"
 def vendedor_or_gestor_required(view_func):
-    decorated_view = user_passes_test(
-        is_vendedor_or_gestor,
-        login_url='/login/',
-        redirect_field_name=None
-    )
-    return decorated_view(view_func)
+    def wrapper(request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return JsonResponse({'error': 'User is not authenticated'}, status=401)
+        if not is_vendedor_or_gestor(request.user):
+            return JsonResponse({'error': 'Permission denied'}, status=403)
+        return view_func(request, *args, **kwargs)
+    return wrapper
 
-
+# Decorator to check if the user is "Gestor"
 def gestor_required(view_func):
-    decorated_view = user_passes_test(
-        is_gestor,
-        login_url='/login/',
-        redirect_field_name=None
-    )
-    return decorated_view(view_func)
+    def wrapper(request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return JsonResponse({'error': 'User is not authenticated'}, status=401)
+        if not is_gestor(request.user):
+            return JsonResponse({'error': 'Permission denied'}, status=403)
+        return view_func(request, *args, **kwargs)
+    return wrapper
