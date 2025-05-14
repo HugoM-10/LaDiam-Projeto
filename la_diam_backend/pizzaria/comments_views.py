8bd98adf4dfd4c5c707ca8da4fc7cb7752e27aa9
Manuel Almeida <66184545+manu183@users.noreply.github.com>
2025-05-14 23:37:18 +0100

    Refactor views to support pagination and enhance request handling for comments, orders, products, ratings, and user profiles

--- la_diam_backend/pizzaria/comments_views.py
+++ la_diam_backend/pizzaria/comments_views.py
@@ -1,49 +1,55 @@
 from .models import Comment, Product
 from .serializers import CommentSerializer
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
 
 
-@api_view(['GET'])
-def get_comments_view(request, product_id=None):
-    if product_id:
-        comments = Comment.objects.filter(product__id=product_id).order_by('-data_publicacao')
-    else:
-        comments = Comment.objects.all().order_by('-data_publicacao')
-    serializer = CommentSerializer(comments, many=True)
-    return Response(serializer.data)
+@permission_classes([IsAuthenticated])
+@api_view(["GET", "POST"])
+def comments_view(request, product_id=None):
 
-@api_view(['GET'])
+    if request.method == "GET":
+        ordering = request.GET.get("ordering", "-data_publicacao")
+
+        product = get_object_or_404(Product, id=product_id)
+        comments = Comment.objects.filter(product=product).order_by(ordering)
+
+        paginator = PageNumberPagination()
+        paginator.page_size = 10
+        result_page = paginator.paginate_queryset(comments, request)
+        serializer = CommentSerializer(result_page, many=True)
+        return Response(serializer.data)
+
+    if request.method == "POST":
+        product_id = request.data.get("product_id")
+        texto = request.data.get("texto")
+
+        if not product_id or not texto:
+            return Response(
+                {"error": "product_id e texto são obrigatórios."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        product = get_object_or_404(Product, id=product_id)
+        comment = Comment.objects.create(
+            user=request.user, product=product, texto=texto
+        )
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def get_my_comments_view(request):
+    ordering = request.GET.get("ordering", "-data_publicacao")
     user = request.user
-    comments = Comment.objects.filter(user=user).order_by('-data_publicacao')
-    serializer = CommentSerializer(comments, many=True)
+    comments = Comment.objects.filter(user=user).order_by(ordering)
+
+    paginator = PageNumberPagination()
+    paginator.page_size = 10
+    result_page = paginator.paginate_queryset(comments, request)
+    serializer = CommentSerializer(result_page, many=True)
     return Response(serializer.data)
-
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def create_comment_view(request):
-    product_id = request.data.get('product_id')
-    texto = request.data.get('texto')
-
-    if not product_id or not texto:
-        return Response({'error': 'product_id e texto são obrigatórios.'}, status=status.HTTP_400_BAD_REQUEST)
-
-    try:
-        product = Product.objects.get(id=product_id)
-    except Product.DoesNotExist:
-        return Response({'error': 'Produto não encontrado.'}, status=status.HTTP_404_NOT_FOUND)
-
-    comment = Comment.objects.create(
-        user=request.user,
-        product=product,
-        texto=texto
-    )
-    serializer = CommentSerializer(comment)
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
--- la_diam_backend/pizzaria/order_views.py
+++ la_diam_backend/pizzaria/order_views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Order, OrderItem
 from .serializers import OrderSerializer, OrderItemSerializer
@@ -14,102 +15,107 @@ from .permissions import (
 )
 
 
-@api_view(["POST"])
+@api_view(["GET", "POST"])
 @permission_classes([IsAuthenticated])
-def create_order_view(request):
-    serializer = OrderSerializer(data=request.data, context={"request": request})
+def orders_view(request):
+    if request.method == "GET":
+        # Get order filtering
+        ordering = request.GET.get("ordering", "-order_date")
 
-    if serializer.is_valid():
-        order = serializer.save()
-        return Response(OrderSerializer(order).data, status=status.HTTP_201_CREATED)
-    
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if is_vendedor_or_gestor(request.user):
+            orders = Order.objects.all().order_by(ordering)
+        else:
+            orders = Order.objects.filter(user=request.user).order_by(ordering)
+
+        paginator = PageNumberPagination()
+        paginator.page_size = 10
+
+        result_page = paginator.paginate_queryset(orders, request)
+        serializer = OrderSerializer(result_page, many=True)
+        return Response(serializer.data)
+
+    elif request.method == "POST":
+        serializer = OrderSerializer(data=request.data, context={"request": request})
+
+        if serializer.is_valid():
+            order = serializer.save()
+            return Response(OrderSerializer(order).data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(["GET"])
+@api_view(["GET", "PUT", "DELETE"])
 @permission_classes([IsAuthenticated])
-def get_orders_view(request):
-    if is_vendedor_or_gestor(request.user):
-        orders = Order.objects.all()
-    else:
-        orders = Order.objects.filter(user=request.user)
+def order_detail(request, order_id):
+    if request.method == "GET":
+        # Check if the order exists or return 404
+        order = get_object_or_404(Order, id=order_id)
 
-    serializer = OrderSerializer(orders, many=True)
-    return Response(serializer.data)
+        if order.user != request.user and not is_vendedor_or_gestor(request.user):
+            return Response(
+                {"error": "You do not have permission to view this order"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
 
+        serializer = OrderSerializer(order)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-@api_view(["GET"])
-@permission_classes([IsAuthenticated])
-def get_order_view(request, order_id):
-    # Check if the order exists or return 404
-    order = get_object_or_404(Order, id=order_id)
+    elif request.method == "PUT":
+        # Check if the order exists or return 404
+        order = get_object_or_404(Order, id=order_id)
 
-    if order.user != request.user and not request.user.is_staff:
+        # Check if the order belongs to the user and if the user is not an admin
+        if order.user != request.user and not is_vendedor_or_gestor(request.user):
+            return Response(
+                {"error": "You do not have permission to edit this order"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+        status_order = request.data.get("status")
+        if status_order:
+            order.status = status_order
+
+        order.save()
         return Response(
-            {"error": "You do not have permission to view this order"},
-            status=status.HTTP_403_FORBIDDEN,
+            {"id": order.id, "status": order.status}, status=status.HTTP_200_OK
         )
+    elif request.method == "DELETE":
+        # Check if the order exists or return 404
+        order = get_object_or_404(Order, id=order_id)
 
-    serializer = OrderSerializer(order)
-    return Response(serializer.data, status=status.HTTP_200_OK)
+        # Check if the order belongs to the user and if the user is not an admin
+        if order.user != request.user and not is_vendedor_or_gestor(request.user):
+            return Response(
+                {"error": "You do not have permission to delete this order"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
 
-
-@api_view(["PUT"])
-@permission_classes([IsAuthenticated])
-def edit_order_view(request, order_id):
-    # Check if the order exists or return 404
-    order = get_object_or_404(Order, id=order_id)
-
-    # Check if the order belongs to the user and if the user is not an admin
-    if order.user != request.user and not request.user.is_staff:
+        order.delete()
         return Response(
-            {"error": "You do not have permission to edit this order"},
-            status=status.HTTP_403_FORBIDDEN,
+            {"message": "Order deleted successfully"}, status=status.HTTP_204_NO_CONTENT
         )
-
-    # Update the order fields
-    status_order = request.data.get("status")
-    if status_order:
-        order.status = status_order
-
-    order.save()
-    return Response({"id": order.id, "status": order.status}, status=status.HTTP_200_OK)
-
-
-@api_view(["DELETE"])
-@permission_classes([IsAuthenticated])
-def delete_order_view(request, order_id):
-    # Check if the order exists or return 404
-    order = get_object_or_404(Order, id=order_id)
-
-    # Check if the order belongs to the user and if the user is not an admin
-    if order.user != request.user and not request.user.is_staff:
-        return Response(
-            {"error": "You do not have permission to delete this order"},
-            status=status.HTTP_403_FORBIDDEN,
-        )
-
-    order.delete()
     return Response(
-        {"message": "Order deleted successfully"}, status=status.HTTP_204_NO_CONTENT
+        {"error": "Invalid request method"}, status=status.HTTP_400_BAD_REQUEST
     )
+
 
 @api_view(["PUT"])
 @vendedor_or_gestor_required
 def update_order_status_view(request, order_id):
-    # Check if the order exists or return 404
     order = get_object_or_404(Order, id=order_id)
-    
-    new_status = request.data.get("status")
 
-    valid_statuses = [choice[0] for choice in Order.STATUS_CHOICES]
 
-    if new_status not in valid_statuses:
+    statuses = ["PENDING", "IN_PROGRESS", "COMPLETED", "CANCELED"]
+    new_status_order = request.data.get("status")
+    if new_status_order in statuses:
+        order.status = new_status_order
+    else:
         return Response(
-            {"error": "Invalid status","possible_statuses": valid_statuses}, status=status.HTTP_400_BAD_REQUEST
+            {"error": "Invalid status", "statuses": statuses}, status=status.HTTP_400_BAD_REQUEST
         )
 
-    order.status = new_status
-
     order.save()
-    return Response({"id": order.id, "status": order.status}, status=status.HTTP_200_OK)
+    return Response(
+        {"id": order.id, "status": order.status}, status=status.HTTP_200_OK
+    )
+    
