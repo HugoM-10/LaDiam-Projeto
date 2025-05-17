a22c9220c40b9431a19790732c45dab4ef7e5e30
Dinis Saraiva <dinis.saraiva.santos@gmail.com>
2025-05-17 16:35:45 +0100

    Added only Gestor can access the page

--- la_diam-frontend/src/Pages/ProductManagement/ProductManager.js
+++ la_diam-frontend/src/Pages/ProductManagement/ProductManager.js
@@ -1,62 +1,90 @@
-import { useState } from "react";
+import { useState, useContext, useEffect } from "react";
 import AddProductForm from "./ProductAdderForm";
 import { Container, Button, ButtonGroup } from "react-bootstrap";
 import ProductList from "./ProductList";
 import EditDeleteProductForm from "./EditDeleteProductForm";
+import { UserContext } from "../../UserContext";
+import { useNavigate } from "react-router-dom";
+import CustomModal from "../../Components/Modal";
 
 const ProductManager = () => {
-    const [selectedOption, setSelectedOption] = useState("add");
-    const [selectedProduct, setSelectedProduct] = useState(null);
+  const [selectedOption, setSelectedOption] = useState("add");
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const navigate = useNavigate();
+  const { userGroup ,user} = useContext(UserContext);
 
-    const handleOptionChange = (option) => {
-        setSelectedOption(option);
-        // Reset the selected product when switching tabs
-        if (option !== "editDelete") {
-            setSelectedProduct(null);
-        }
-    };
+  // Modal state
+  const [modalShow, setModalShow] = useState(false);
+  const [modalMsg, setModalMsg] = useState("");
+  const [modalTitle, setModalTitle] = useState("Aviso");
+  useEffect(() => {
+    console.log(userGroup);
+    if (userGroup !== "Gestor") {
+      setModalTitle("Atenção");
+      setModalMsg("É preciso ser gestor para aceder a esta página.");
+      setModalShow(true);
+      setTimeout(() => {
+        setModalShow(false);
+        navigate("/");
+      }, 1800);
+    }
+  }, [user]);
 
-    // Callback used after add/edit/delete to return to the product list.
-    const returnToProductList = () => {
-        setSelectedOption("editDelete");
-        setSelectedProduct(null);
-    };
+  const handleOptionChange = (option) => {
+    setSelectedOption(option);
+    if (option !== "editDelete") {
+      setSelectedProduct(null);
+    }
+  };
+  const returnToProductList = () => {
+    setSelectedOption("editDelete");
+    setSelectedProduct(null);
+  };
 
-    return (
-        <Container className="my-4">
-            <h1 className="mb-4">Gestão de Produtos</h1>
+  return (
+    <Container className="my-4">
+      <h1 className="mb-4">Gestão de Produtos</h1>
 
-            <ButtonGroup className="mb-4">
-                <Button
-                    variant={selectedOption === "add" ? "primary" : "secondary"}
-                    onClick={() => handleOptionChange("add")}
-                >
-                    Adicionar Produto
-                </Button>
-                <Button
-                    variant={selectedOption === "editDelete" ? "primary" : "secondary"}
-                    onClick={() => handleOptionChange("editDelete")}
-                >
-                    Editar/Apagar Produto
-                </Button>
-            </ButtonGroup>
+      <ButtonGroup className="mb-4">
+        <Button
+          variant={selectedOption === "add" ? "primary" : "secondary"}
+          onClick={() => handleOptionChange("add")}
+        >
+          Adicionar Produto
+        </Button>
+        <Button
+          variant={selectedOption === "editDelete" ? "primary" : "secondary"}
+          onClick={() => handleOptionChange("editDelete")}
+        >
+          Editar/Apagar Produto
+        </Button>
+      </ButtonGroup>
 
-            <div>
-                {selectedOption === "add" && (
-                    <AddProductForm onProductChanged={returnToProductList} />
-                )}
-                {selectedOption === "editDelete" && (
-                    <div>
-                        {!selectedProduct ? (
-                            <ProductList onEditSelect={setSelectedProduct} />
-                        ) : (
-                            <EditDeleteProductForm product={selectedProduct} onProductChanged={returnToProductList} />
-                        )}
-                    </div>
-                )}
-            </div>
-        </Container>
-    );
+      <div>
+        {selectedOption === "add" && (
+          <AddProductForm onProductChanged={returnToProductList} />
+        )}
+        {selectedOption === "editDelete" && (
+          <div>
+            {!selectedProduct ? (
+              <ProductList onEditSelect={setSelectedProduct} />
+            ) : (
+              <EditDeleteProductForm
+                product={selectedProduct}
+                onProductChanged={returnToProductList}
+              />
+            )}
+          </div>
+        )}
+      </div>
+      <CustomModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        modalTitle={modalTitle}
+        modalMsg={modalMsg}
+      />
+    </Container>
+  );
 };
 
 export default ProductManager;
